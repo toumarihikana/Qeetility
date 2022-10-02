320244d561acc44c01957fd37f9301a1ca8f1123
ToumariHikana <toumarihikana@gmail.com>
2022-10-03 03:06:59 +0900

    環境構築

---
+++ bin/main/Exsample.scala
@@ -0,0 +1,30 @@
+package com.ambersyndrome.qeetility.exsample
+
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.text.ITextComponent
+import net.minecraft.util.text.TextComponentString
+import net.minecraftforge.common.MinecraftForge
+//import net.minecraftforge.event.entity.player.PlayerEvent
+import net.minecraftforge.fml.common.gameevent.PlayerEvent
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+
+class LoginMessage(){
+  @SubscribeEvent
+  def onPlayerLoggedIn(event: PlayerEvent.PlayerLoggedInEvent): Unit = {
+    val player = event.player
+    val pos = player.getPosition
+    val classpath = Thread.currentThread.getContextClassLoader.getResource("").getPath
+    System.out.println("classpath : " + classpath)
+    val message = s"""
+                     |Hello, World
+                     |name = [${player.getName}]
+                     |pos  = [${pos.getX}. ${pos.getY}, pos.getZ]
+                     |mes  = [${classpath}]
+    """.stripMargin
+    player.sendMessage(new TextComponentString(message))
+//    , player.getUniqueID)
+  }
+}
